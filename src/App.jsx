f621d411ab676e255d36fa7b95e352c4550975e2
Avinash-yadav103 <avinash.yadav02102004@gmail.com>
2024-08-08 20:32:00 +0530

    first

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import React from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Nav from './components/Nav.jsx'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Nav />
+      {/* <p>Hello there</p> */}
+    </>
+  )
+}
+
+export default App
