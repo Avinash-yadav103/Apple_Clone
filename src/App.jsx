b5394ed9cf09e9d5e160689d7878370cb19de653
Avinash-yadav103 <avinash.yadav02102004@gmail.com>
2024-08-09 15:21:08 +0530

    hero

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import React from 'react'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Nav from './components/Nav.jsx'
+import Hero from './components/Hero.jsx'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      <Nav />
-      {/* <p>Hello there</p> */}
+      <div className="whole">
+        <Nav />
+        {/* <p>Hello there</p> */}
+        <Hero price="From ₹59900.00*" />
+      </div>
     </>
   )
 }
---
+++ src/components/Hero.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import logo from './ipadairlogo.png'
+import mainipad from './main_ipad.png'
+
+function Hero(props) {
+  return (
+    <div className='hero'>
+      <div className="ipadair">
+        <div className="image_logo">
+            <img src={logo} alt="" />
+        </div>
+        <div className="freshair">
+            <p>Fresh Air</p>
+            <img src={mainipad} alt="" />
+        </div>
+        <div className="buy_button">
+            <button>Buy</button>
+        </div>
+        <p>{props.price}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
